84d76d4dfd5f8972783732e7fe9edd88c58afd47
Nurul Amin Limon <nurulaminlimon261893@gmail.com>
2020-03-22 20:48:07 +0600

    website blogs lists added

--- Index.php
+++ Index.php
@@ -1,3 +1,11 @@
+<?php 
+	$con  = mysqli_connect("localhost","root","","blogs");
+	if (!$con) {
+		die("connection faild");
+	}
+
+ ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,7 +18,35 @@
 <body>
 
 	<div class="container">
-		
+		<div class="jumbotron">
+			<div class="row">
+				<div class="col-md-12 bg-success">
+					<h1 class="display-1 text-center text-light">Blog with CSE</h1>
+				</div>
+			</div>
+			<div class="row  p-2">
+				<?php 
+				$query = "SELECT posts.*, catagories.Title as tit
+							FROM posts
+							JOIN catagories 
+							ON posts.Catagoris_id = catagories.Id";
+
+				$results =  mysqli_query($con, $query);
+				while ($row = mysqli_fetch_assoc($results)) { ?>
+					<div class="col-md-8 offset-md-3 bg-danger mb-3 mt-2">
+						<h1 class="text-center p-3"><?php echo $row['Tittle']; ?></h1>
+						<img src="admin/posts/<?php echo $row['Images']; ?>" class="float-left  m-2" alt="image" width="120" height="120">
+						<p class="lead text-justify"><?php echo substr( $row['Description'],0, 150); ?></p>
+						<div class="row p-2 justify-content-end">
+							<a href="" class="btn btn-success">Read more</a>
+						</div>
+					</div>
+			<?php	}
+				?>
+				
+			</div>
+			
+		</div>
 	</div>
 
 
